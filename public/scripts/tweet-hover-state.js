c1de0e9ccde257a947e9c27d1917e7115a4aa484
Kristin <kvcmorin@gmail.com>
2017-03-23 19:52:21 +0000

    used ajax to get/post tweets in the event submit handler, added validations for form submission and added window reload so refresh happens auto

--- public/scripts/tweet-hover-state.js
+++ public/scripts/tweet-hover-state.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $("article.tweet").on("mouseover", function(event) {
+  $("#appendNewTweetsHere").on("mouseover", function(event) {
     $(this).css("font-weight", "bold");
     $("section h6").css("color", "#0e1740");
   });
-  $("article.tweet").on("mouseout", function(event) {
+  $("#appendNewTweetsHere").on("mouseout", function(event) {
     $(this).css("font-weight", "normal");
     $("article.tweet h6").css("color", "#97989d")
   });
